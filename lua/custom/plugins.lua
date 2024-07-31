1bdc98ecc0e070cb5ecbdb9d840df4c0d479296f
Thomas Sedlmair <thomas.sedlmair@googlemail.com>
2024-07-31 16:17:09 +0200

    update

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -115,12 +115,26 @@ local plugins = {
   },
   {
     "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  }
+    requires = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    enabled = true,
+    config = function()
+        require("todo-comments").setup {
+          keywords = {
+            FIX = {
+                icon = " ", -- icon used for the sign, and in search results
+                color = "error", -- can be a hex color, or a named color (see below)
+                alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+                -- signs = false, -- configure signs for some keywords individually
+            },
+            TODO = { icon = " ", color = "info" },
+            HACK = { icon = " ", color = "warning" },
+            WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+            PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+            NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+        },
+      }
+    end,
+  },
 }
 return plugins
