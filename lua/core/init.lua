7cb1fce5861ebe1a2714d3cda2482bfdb7ae2ba5
Thomas Sedlmair <thomas.sedlmair@googlemail.com>
2026-03-13 09:21:41 +0100

    changed foldmethod

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -11,7 +11,7 @@ g.transparency = config.ui.transparency
 -------------------------------------- options ------------------------------------------
 
 opt.foldlevel = 99
-opt.foldmethod = "syntax"
+opt.foldmethod = "manual"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- Define a function to show help for fold commands
