06a67fd510caccd2dc9c8ebc3df49f4fadc630c9
Thomas Sedlmair <thomas.sedlmair@googlemail.com>
2026-03-17 07:46:56 +0000

    added rainbow delimiters

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -267,5 +267,31 @@ local plugins = {
       })
     end
   },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local rainbow_delimiters = require("rainbow-delimiters")
+
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = rainbow_delimiters.strategy.global,
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      }
+    end,
+  },
 }
 return plugins
