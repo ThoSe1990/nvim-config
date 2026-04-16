3cd79323fba8487a245d771908be9f0cc5612ccb
Thomas <thomas.sedlmair@googlemail.com>
2026-04-16 09:27:33 +0200

    foldmethod indent

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
-    vim.opt.foldmethod = "expr"
+    vim.opt.foldmethod = "indent"
     vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     vim.opt.foldlevel = 99
   end,
