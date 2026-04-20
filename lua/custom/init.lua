3ccfe89f4c506a7384973f491ea909ce17fd04f5
Thomas <thomas.sedlmair@googlemail.com>
2026-04-20 09:42:54 +0200

    added oil autocmds

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -6,3 +6,31 @@ vim.api.nvim_create_autocmd("BufEnter", {
     vim.opt_local.foldlevel = 99
   end,
 })
+
+
+-- Oil: Enter open directory in Oil explorer
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "oil",
+  callback = function()
+    local oil = require("oil")
+
+    vim.keymap.set("n", "<CR>", function()
+      oil.select()
+    end, { buffer = true, desc = "Oil: enter directory / open file" })
+  end,
+})
+-- Oil: Esace closes Oil file explorer
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "oil",
+  callback = function()
+    local oil = require("oil")
+
+    vim.keymap.set("n", "<CR>", function()
+      oil.select()
+    end, { buffer = true, desc = "Oil: open / enter directory" })
+
+    vim.keymap.set("n", "<Esc>", function()
+      oil.close()
+    end, { buffer = true, desc = "Oil: close" })
+  end,
+})
